5b2cfb671ed7925fe9f81357888cf0fd74ea861c
Paulo Vitor Lima <paulovitorbal@gmail.com>
2017-05-29 22:20:29 -0300

    adicionado escolha de meses e ajustes de impressao

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
 require_once('calendario.php');
-$f = new Calendario();
-//$datas = $f->getDatas();
-$mes = $f->getMes();
+
+
+$explode = explode('/', $_SERVER['REQUEST_URI']);
+$mesInformado = $anoInformado = null;
+if (array_key_exists(1, $explode))
+	$mesInformado = (int) $explode[1];
+if (array_key_exists(2, $explode))
+	$anoInformado = (int) $explode[2];
+if ($mesInformado == "")
+	$mesInformado = (int) date('m');
+if ($anoInformado == "")
+	$anoInformado = (int) date('Y');
+
+$f = new Calendario($anoInformado);
+$mes = $f->getMes($mesInformado);
+
+
 
 
 include('main.phtml');
