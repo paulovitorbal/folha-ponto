fb80e3687ef28bb5b4f99fc53c4fa50ec1276067
Paulo Vitor Bettini de Paiva Lima <paulovitorbal@gmail.com>
2017-06-27 12:28:19 -0300

    alteracao no mapeamento das rotas

--- router.php
+++ router.php
@@ -5,10 +5,10 @@ require __DIR__ . '/vendor/autoload.php';
 // Create Router instance
 $router = new \Bramus\Router\Router();
 
-$router->match('GET|POST', '/', function() { 
+$router->match('GET|POST', '/.*', function() { 
 	require('index.php');
 });
-$router->match('GET|POST', '/vale-transporte', function() { 
+$router->match('GET|POST', '/vale-transporte/?.*', function() { 
 	require('vale-transporte.php');
 });
 
