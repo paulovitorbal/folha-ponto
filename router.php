165f3226099da00940b6563734e734a9594dbde3
Paulo Vitor Bettini de Paiva Lima <paulovitorbal@gmail.com>
2017-06-27 12:21:33 -0300

    bramus router

---
+++ router.php
@@ -0,0 +1,15 @@
+<?php
+// Require composer autoloader
+require __DIR__ . '/vendor/autoload.php';
+
+// Create Router instance
+$router = new \Bramus\Router\Router();
+
+$router->match('GET|POST', '/', function() { 
+	require('index.php');
+});
+$router->match('GET|POST', '/vale-transporte', function() { 
+	require('vale-transporte.php');
+});
+
+$router->run();
